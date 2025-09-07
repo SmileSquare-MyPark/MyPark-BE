717e8dc5174b75adaeb4e5fe15a72f60858c0581
Jeongjun Min <m3k0813@gmail.com>
2025-09-07 14:34:11 +0900

    chore: Swagger 추가

---
+++ src/main/java/com/smile/mypark/global/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.smile.mypark.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		Info info = new Info()
+			.version("v1.0")
+			.title("MyPark API")
+			.description("MyPark Project API");
+		return new OpenAPI()
+			.info(info);
+	}
+}

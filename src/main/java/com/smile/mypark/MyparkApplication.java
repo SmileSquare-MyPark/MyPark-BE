6ac7117ab0ede06209eccbca562d34d20b66642c
hangunhee39 <ghee0831@naver.com>
2025-09-07 13:15:32 +0900

    init

---
+++ src/main/java/com/smile/mypark/MyparkApplication.java
@@ -0,0 +1,13 @@
+package com.smile.mypark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smile/mypark/MyparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smile.mypark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

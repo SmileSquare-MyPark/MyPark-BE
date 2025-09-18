fe12087064804c6d77ff9d3c80d605fb29a5826a
Jeongjun Min <m3k0813@gmail.com>
2025-09-18 22:50:11 +0900

    feat: 회원가입 구현

--- src/main/java/com/smile/mypark/domain/user/service/UserService.java
+++ src/main/java/com/smile/mypark/domain/user/service/UserService.java
@@ -1,7 +1,11 @@
 package com.smile.mypark.domain.user.service;
 
+import com.smile.mypark.domain.user.dto.request.CreateUserRequestDTO;
 import com.smile.mypark.domain.user.dto.response.UserResponseDTO;
+import com.smile.mypark.domain.user.entity.User;
 
 public interface UserService {
-	UserResponseDTO getUser(Long userId);
+	User createUser(CreateUserRequestDTO request);
+
+	UserResponseDTO getUser(Long idx);
 }
--- src/main/java/com/smile/mypark/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/smile/mypark/domain/user/service/UserServiceImpl.java
@@ -1,9 +1,13 @@
 package com.smile.mypark.domain.user.service;
 
+import java.time.LocalDateTime;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.smile.mypark.domain.user.converter.UserConverter;
+import com.smile.mypark.domain.user.dto.request.CreateUserRequestDTO;
 import com.smile.mypark.domain.user.dto.response.UserResponseDTO;
 import com.smile.mypark.domain.user.entity.User;
 import com.smile.mypark.domain.user.repository.UserRepository;
@@ -18,10 +22,35 @@ import lombok.RequiredArgsConstructor;
 public class UserServiceImpl implements UserService {
 
 	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
 
 	@Override
-	public UserResponseDTO getUser(Long userId) {
-		User user = userRepository.findByIdAndNotDeleted(userId)
+	@Transactional
+	public User createUser(CreateUserRequestDTO request) {
+		String encodedPassword = passwordEncoder.encode(request.getPassword());
+
+		User user = User.builder()
+			.uId(request.getUId())
+			.password(encodedPassword)
+			.nickname(request.getNickname())
+			.kind(request.getKind())
+			.height(request.getHeight())
+			.weight(request.getWeight())
+			.age(request.getAge())
+			.isAgreePos(request.getIsAgreePos())
+			.isAgreeAlert(request.getIsAgreeAlert())
+			.uIdx(request.getUIdx())
+			.regDate(LocalDateTime.now())
+			.lastDate(LocalDateTime.now())
+			.build();
+
+		return userRepository.save(user);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public UserResponseDTO getUser(Long idx) {
+		User user = userRepository.findByuIdx(idx)
 			.orElseThrow(() -> new GeneralException(ErrorStatus._USER_NOT_FOUND));
 
 		return UserConverter.toUserResponseDTO(user);

daaf7e8a3e06199c74d4b954fd3ab0f346072484
Jeongjun Min <m3k0813@gmail.com>
2025-10-01 09:02:13 +0900

    fix: SMS 메세지 포멧 변경

--- src/main/java/com/smile/mypark/global/util/SmsCertificationUtil.java
+++ src/main/java/com/smile/mypark/global/util/SmsCertificationUtil.java
@@ -34,7 +34,7 @@ public class SmsCertificationUtil {
 			Message message = new Message();
 			message.setFrom(fromNumber);
 			message.setTo(to);
-			message.setText("[마이파크] 본인확인 인증번호는 " + certificationCode + "입니다.");
+			message.setText("[마이파크] 본인확인 인증번호는 " +  "[" + certificationCode + "]" + " 입니다.");
 
 			this.messageService.sendOne(new SingleMessageSendingRequest(message));
 		} catch (Exception e) {
